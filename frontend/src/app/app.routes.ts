07e4bc538329ff018b4fdb51f0d58702b0d976bb
haris <hkidds23@gmail.com>
2024-02-09 16:29:34 +0100

    debut router

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import {FirstComponent} from './first/first.component';
+import {SecondComponent} from './second/second.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent},
+    { path: 'page1', component: FirstComponent },
+    { path: 'page2', component: SecondComponent },
+];
