0e03b0daf93c254f64466b800bc20133c622e1dc
haris <hkidds23@gmail.com>
2024-02-14 15:03:30 +0100

    div(click)

--- frontend/src/app/menu/menu.component.ts
+++ frontend/src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
@@ -8,5 +9,10 @@ import { Component } from '@angular/core';
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  constructor(private router: Router) { }
+
+  goToHome() {
+    this.router.navigateByUrl('/');
+  }
 
 }
