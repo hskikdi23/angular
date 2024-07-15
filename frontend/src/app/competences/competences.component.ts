5a7591468a838718b4fdb048abfcb8e50de3d0c4
hskikdi <haris.skikdi@gmail.com>
2024-07-15 20:48:19 +0200

    refonte page competences

--- frontend/src/app/competences/competences.component.ts
+++ frontend/src/app/competences/competences.component.ts
@@ -1,19 +1,20 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-competences',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './competences.component.html',
   styleUrl: './competences.component.css'
 })
 export class CompetencesComponent {
 
   icons = [
-    {src : '../../assets/angularIcon.png', alt:'angularIcon'},
+    {src : '../../assets/angular-icon-logo.png', alt:'angularIcon'},
     {src : '../../assets/cplus.png', alt:'cplusIcon'},
     {src : '../../assets/dockerIcon.png', alt:'dockerIcon'},
-    {src : '../../assets/jsIcon.png', alt:'jsIcon'},
+    {src : '../../assets/vecteezy_javascript-logo-png-javascript-icon-transparent-png_27127463.png', alt:'jsIcon'},
     {src : '../../assets/nestIcon.png', alt:'nestIcon'},
     {src : '../../assets/postgressIcon.png', alt:'pgIcon'},
     {src : '../../assets/svelteIcon.png', alt:'svelteIcon'},
